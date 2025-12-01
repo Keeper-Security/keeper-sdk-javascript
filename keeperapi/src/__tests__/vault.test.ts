b499069c9583003a74402d6fc635ab091fa675ce
Hoseong Lee <154545063+hleekeeper@users.noreply.github.com>
2025-12-01 14:27:13 -0600

    BE-7008 Added unit tests for owned records created, updated, and deleted via sync down (#95)
    * Added unit test cases for owned records added/edited/deleted through sync down routine
    
    * Added the test:syncDown command and registered it in the CI/CD pipeline
    
    * use the node platform instead of browser platform
    
    * upgraded the node version to 20 in Github Action
    
    * Removed the testUtils module - unneccessary
    
    * updated the test case messages
    
    * updated a test case of the Owned Records

---
+++ keeperapi/src/__tests__/vault.test.ts
@@ -0,0 +1,151 @@
+import {syncDown, VaultStorage} from "../vault"
+import {Auth} from '../auth'
+import {nodePlatform} from "../node/platform"
+import {connectPlatform, platform} from "../platform";
+import {SyncDownResponseBuilder} from './SyncDownResponseBuilder'
+import {Records, Vault} from "../proto";
+import {webSafe64FromBytes} from "../utils";
+
+describe('Sync Down', () => {
+  let dataKey: Uint8Array
+  let auth: Auth;
+  let eccKeyPair: {privateKey: Uint8Array, publicKey: Uint8Array};
+  let storage: VaultStorage;
+  let mockSyncDownCommand: jest.MockedFunction<() => any>;
+  let syncDownResponseBuilder: SyncDownResponseBuilder;
+  beforeAll(async () => {
+    connectPlatform(nodePlatform)
+    dataKey = platform.getRandomBytes(32)
+    eccKeyPair = await platform.generateECKeyPair()
+  })
+  describe('Owned Records', () => {
+    beforeEach(() => {
+      storage = {
+        get: jest.fn(),
+        addDependencies: jest.fn(),
+        delete: jest.fn(),
+        removeDependencies: jest.fn(),
+        put: jest.fn(),
+        saveObject: jest.fn(),
+        saveKeyBytes: jest.fn(),
+      } as unknown as VaultStorage;
+      mockSyncDownCommand = jest.fn()
+      auth = {
+        dataKey,
+        eccPrivateKey: eccKeyPair.privateKey,
+        eccPublicKey: eccKeyPair.publicKey,
+        executeRest: mockSyncDownCommand,
+      } as unknown as Auth;
+      syncDownResponseBuilder = new SyncDownResponseBuilder();
+    })
+    it('saves the record data when a new record is created by the user', async () => {
+      const decryptedRecordKey = platform.getRandomBytes(32)
+      const recordKey = await platform.aesGcmEncrypt(decryptedRecordKey, auth.dataKey!)
+      const recordUid = platform.getRandomBytes(16)
+      const recordUidStr = webSafe64FromBytes(recordUid)
+      const decryptedRecordData = {
+        title: 'test record',
+      }
+      const decodedRecordData = platform.stringToBytes(JSON.stringify(decryptedRecordData))
+      const recordData = await platform.aesGcmEncrypt(decodedRecordData, decryptedRecordKey)
+      const userFolderRecord: Vault.IUserFolderRecord = {
+        recordUid,
+        revision: 1,
+      }
+      const recordMetadata: Vault.IRecordMetaData = {
+        recordUid,
+        recordKey,
+        owner: true,
+        canEdit: true,
+        canShare: true,
+        recordKeyType: Records.RecordKeyType.ENCRYPTED_BY_DATA_KEY_GCM,
+        ownerUsername: 'hlee+dev@keepersecurity.com',
+        ownerAccountUid: platform.getRandomBytes(16),
+      }
+      const record: Vault.IRecord = {
+        recordUid,
+        version: 3,
+        data: recordData,
+        extra: new Uint8Array([]),
+      }
+      syncDownResponseBuilder
+        .addUserFolderRecord(userFolderRecord)
+        .addRecordMetadata(recordMetadata)
+        .addRecord(record)
+      mockSyncDownCommand.mockResolvedValue(syncDownResponseBuilder.build())
+      await syncDown({
+        auth,
+        storage,
+      })
+      expect(storage.put).toHaveBeenCalledWith(
+        expect.objectContaining({
+          kind: 'metadata',
+          uid: recordUidStr,
+          owner: recordMetadata.owner,
+          ownerUsername: recordMetadata.ownerUsername,
+        })
+      )
+      expect(storage.put).toHaveBeenCalledWith(
+        expect.objectContaining({
+          kind: 'record',
+          uid: recordUidStr,
+          data: decryptedRecordData,
+        })
+      )
+      expect(storage.addDependencies).toHaveBeenCalledWith({
+        "": new Set([{
+          kind: "record",
+          "parentUid": "",
+          uid: recordUidStr,
+        }])
+      })
+    })
+
+    it('saves the new record data when an existing record is updated by the user', async () => {
+      const decryptedRecordKey = platform.getRandomBytes(32)
+      const recordKey = await platform.aesGcmEncrypt(decryptedRecordKey, auth.dataKey!)
+      const recordUid = platform.getRandomBytes(16)
+      const recordUidStr = webSafe64FromBytes(recordUid)
+      await platform.unwrapKey(recordKey, recordUidStr, 'data', 'gcm', 'aes')
+      const decryptedRecordData = {
+        title: 'test record updated',
+      }
+      const decodedRecordData = platform.stringToBytes(JSON.stringify(decryptedRecordData))
+      const recordData = await platform.aesGcmEncrypt(decodedRecordData, decryptedRecordKey)
+
+      const record: Vault.IRecord = {
+        recordUid,
+        version: 3,
+        data: recordData,
+        extra: new Uint8Array([]),
+      }
+      syncDownResponseBuilder
+        .addRecord(record)
+      mockSyncDownCommand.mockResolvedValue(syncDownResponseBuilder.build())
+      await syncDown({
+        auth,
+        storage,
+      })
+      expect(storage.put).toHaveBeenCalledWith(
+        expect.objectContaining({
+          kind: 'record',
+          uid: recordUidStr,
+          data: decryptedRecordData,
+        })
+      )
+    })
+
+    it('deletes the record data when an existing record is removed', async () => {
+      const recordUid = platform.getRandomBytes(16)
+      syncDownResponseBuilder
+        .addRemovedRecord(recordUid)
+      mockSyncDownCommand.mockResolvedValue(syncDownResponseBuilder.build())
+      await syncDown({
+        auth,
+        storage,
+      })
+      expect(storage.delete).toHaveBeenCalledWith('record', webSafe64FromBytes(recordUid))
+    })
+  })
+})
+
