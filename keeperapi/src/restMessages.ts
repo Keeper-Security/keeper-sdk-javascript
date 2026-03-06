539886177e1a5db335b5208b3fdbfb9e042f0623
THeflinKeeper <theflin@keepersecurity.com>
2026-03-05 16:43:36 -0800

    fixed message

--- keeperapi/src/restMessages.ts
+++ keeperapi/src/restMessages.ts
@@ -16,7 +16,6 @@ import {
     NotificationCenter,
     Folder,
     record,
-    records,
     folder
 } from './proto'
 
@@ -452,8 +451,8 @@ export const V3RecordAddMessage = (data: record.v3.RecordsAddRequest): RestMessa
 export const V3RecordUpdateMessage = (data: Records.RecordsUpdateRequest): RestMessage<Records.IRecordsUpdateRequest, Records.IRecordsModifyResponse> =>
     createMessage(data, 'vault/records/v3/update', Records.RecordsUpdateRequest, Records.RecordsModifyResponse)
 
-export const V3RecordShareMessage = (data: records.share.v3.Request): RestMessage<records.share.v3.IRequest, records.share.v3.IResponse> =>
-    createMessage(data, 'vault/records/v3/share', records.share.v3.Request, records.share.v3.Response)
+export const V3RecordShareMessage = (data: record.v3.sharing.Request): RestMessage<record.v3.sharing.IRequest, record.v3.sharing.IResponse> =>
+    createMessage(data, 'vault/records/v3/share', record.v3.sharing.Request, record.v3.sharing.Response)
 
 export const V3RecordDetailsMessage = (data: record.v3.details.RecordDataRequest): RestMessage<record.v3.details.IRecordDataRequest, record.v3.details.IRecordDataResponse> =>
     createMessage(data, 'vault/records/v3/details/data', record.v3.details.RecordDataRequest, record.v3.details.RecordDataResponse)
