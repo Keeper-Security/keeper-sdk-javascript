96fc6154606ab6d52c59f2d6397a466db8fb1230
ukumar-ks <ukumar@keepersecurity.com>
2026-05-05 10:03:56 +0530

    Undo CLIENT_VERSION  'w18.2.0' back to 'c17.0.0'

--- KeeperSdk/src/utils/constants.ts
+++ KeeperSdk/src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const SdkDefaults = {
-    CLIENT_VERSION: 'w18.2.0',
+    CLIENT_VERSION: 'c17.0.0',
     DEVICE_NAME: 'JavaScript Keeper SDK',
     CONFIG_DIR: '.keeper',
     LOG_FORMAT: '!',
