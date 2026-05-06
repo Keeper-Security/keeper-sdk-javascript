3e9492905d141ef6f97bf34af9777fba70a25e54
Hoseong Lee <154545063+hleekeeper@users.noreply.github.com>
2026-05-06 16:43:21 -0500

    BE-6875 Additional changes to ingest KD folder data (#135)
    * minor change uid -> folderUid in the DKdFolderAccesst type
    
    * A removed dependency can now explicitly specify parentKind and childKind

--- keeperapi/src/vault.ts
+++ keeperapi/src/vault.ts
@@ -290,9 +290,14 @@ export type Dependency = {
     parentUid: string
     uid: string
 }
+export type RemovedDependency = {
+    parentKind: VaultStorageKind
+    childKind: VaultStorageKind
+    childUid: string
+}
 export type DependencyMap = Record<string, Dependency>
 export type Dependencies = Record<string, Set<Dependency>>
-export type RemovedDependencies = Record<string, Set<string> | '*'>
+export type RemovedDependencies = Record<string, Set<string | RemovedDependency> | '*'>
 
 const addDependencies = (dependencies: Dependencies, parentUid: string, childUid: string, kind: VaultStorageKind) => {
     let children = dependencies[parentUid]
@@ -307,13 +312,17 @@ const addDependencies = (dependencies: Dependencies, parentUid: string, childUid
     })
 }
 
-const addRemovedDependencies = (dependencies: RemovedDependencies, parentUid: string, childUid: string) => {
+const addRemovedDependencies = (
+    dependencies: RemovedDependencies,
+    parentUid: string,
+    childUid: string | RemovedDependency
+) => {
     let children = dependencies[parentUid]
     if (children === '*') {
         return
     }
     if (!children) {
-        children = new Set<string>()
+        children = new Set<string | RemovedDependency>()
         dependencies[parentUid] = children
     }
     children.add(childUid)
@@ -1216,7 +1225,7 @@ const processKdFolderAccesses = async (
         await storage.put({
             kind: 'keeper_drive_folder_access',
             accessUid: createKdFolderAccessCompositeKey(accessTypeUid, folderUid),
-            uid: folderUid,
+            folderUid,
             accessTypeUid,
             accessType: folderAccess.accessType,
             accessRoleType: folderAccess.accessRoleType,
@@ -1425,7 +1434,11 @@ const processKdRemovedFolderRecords = (
         if (!folderRecord.recordUid || !folderRecord.folderUid) continue
         const recordUid = webSafe64FromBytes(folderRecord.recordUid)
         const folderUid = webSafe64FromBytes(folderRecord.folderUid)
-        addRemovedDependencies(removedDependencies, folderUid, recordUid)
+        addRemovedDependencies(removedDependencies, folderUid, {
+            parentKind: 'keeper_drive_folder',
+            childKind: 'record',
+            childUid: recordUid,
+        })
     }
 }
 
